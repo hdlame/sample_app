260c839610d4d7b50b23b9850dc4b9e2fe90d7c0
Hajer Dlame <hdlame@brotherhoodmutual.com>
2012-07-10 14:01:26 -0400

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
+   describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+      it "should have tge title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App | About Us')
+
+    end
+  end
+end
